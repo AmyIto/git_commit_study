9106984eeb6c5f903e84d095039faf5ad943e25e
AmyIto <ito.emi.web@gmail.com>
2020-10-21 14:20:16 +0900

    first commit

---
+++ display.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>課題1</title>
+</head>
+<body>
+<?php echo $_GET['name']; ?>
+</body>
+</html>
---
+++ form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>課題1</title>
+</head>
+<body>
+<form action="display.php" method="get">
+<input type="text" name="name">
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
